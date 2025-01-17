2416646880880d4cdec7c1a9482e40dc9e8d3ac7
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2025-01-18 04:04:23 +0600

    40 [Front-End] [Home Page] Working With Brands Categories Features Component

--- client/src/components/features/features.jsx
+++ client/src/components/features/features.jsx
@@ -1,17 +1,44 @@
 import React from "react";
 import FeatureStore from "../../store/FeatureStore";
-import FeaturesSkeleton from './../../skeleton/features-skeleton';
+import FeaturesSkeleton from "./../../skeleton/features-skeleton";
 
 const Features = () => {
-    const { FeatureList } = FeatureStore();
+  const { FeatureList } = FeatureStore();
 
-    if (FeatureList === null){
-        return <FeaturesSkeleton/>
-      }else{
-        return (
-          <div>FeatureList</div>
-        )
-      }
+  if (FeatureList === null) {
+    return <FeaturesSkeleton />;
+  } else {
+    return (
+      <>
+        <div className="container section">
+          <div className="row">
+            {FeatureList.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className="col-6 p-2 col-md-3 col-lg-3 col-sm-6"
+                >
+                  <div className="card shadow-sm">
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col-3">
+                          <img className="w-100" alt="image" src={item['img']} />
+                        </div>
+                        <div className="col-9">
+                          <h3 className="bodyXLarge">{item['name']}</h3>
+                          <span className="bodySmal">{item['description']}</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </>
+    );
+  }
 };
 
 export default Features;
