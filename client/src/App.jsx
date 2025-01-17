e6397a26713c4262824cdf72721ac67c656799c4
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2025-01-18 02:35:41 +0600

    38 [Front-End] [Home Page] Create Store And API Call

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
+        <Route exact path="/" element={<HomePage/>} />
       </Routes>
     </BrowserRouter>
   );
