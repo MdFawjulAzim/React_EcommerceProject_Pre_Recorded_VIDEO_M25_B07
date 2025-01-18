6e4f188e576a08fa7f4fcb9bbcd9abb02c313b92
Md. Fawjul Azim <mdfawjulazim617@gmail.com>
2025-01-18 08:13:27 +0600

    47 [Front-End] [Details Page] Working With Product Details Components

--- client/src/components/product/ProductImages.jsx
+++ client/src/components/product/ProductImages.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import ProductStore from "../../store/ProductStore";
+import ImageGallery from "react-image-gallery";
+import "react-image-gallery/styles/css/image-gallery.css";
 
 const ProductImages = () => {
-  return (
-    <div>ProductImages</div>
-  )
-}
+  const { Details } = ProductStore();
+  let images = [
+    {original: Details[0]['details']['img1'], thumbnail: Details[0]['details']['img1']},
+    {original: Details[0]['details']['img2'], thumbnail: Details[0]['details']['img2']},
+    {original: Details[0]['details']['img3'], thumbnail: Details[0]['details']['img3']},
+    {original: Details[0]['details']['img4'], thumbnail: Details[0]['details']['img4']},
+    {original: Details[0]['details']['img5'], thumbnail: Details[0]['details']['img5']},
+    {original: Details[0]['details']['img6'], thumbnail: Details[0]['details']['img6']},
+    {original: Details[0]['details']['img7'], thumbnail: Details[0]['details']['img7']},
+    {original: Details[0]['details']['img8'], thumbnail: Details[0]['details']['img8']},
+];
 
-export default ProductImages
+  return (
+    <div>
+      {" "}
+      <ImageGallery autoPlay={true} items={images} />
+    </div>
+  );
+};
+
+export default ProductImages;
--- client/src/components/product/details.jsx
+++ client/src/components/product/details.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import ProductImages from "./ProductImages.jsx";
 import ProductStore from "../../store/ProductStore.js";
 import DetailsSkeleton from "../../skeleton/details-skeleton.jsx";
+import parse from 'html-react-parser';
 
 const Details = () => {
   const { Details, ReviewList } = ProductStore();
+  const [quantity, SetQuantity] = useState(1);
+
+  const incrementQunatity = () => {
+    SetQuantity(quantity=>quantity + 1);
+  };
+
+  const decrementQunatity = () => {
+    if (quantity > 1) SetQuantity(quantity - 1);
+  };
 
   if (Details === null) {
     return <DetailsSkeleton />;
@@ -18,36 +28,59 @@ const Details = () => {
                 <ProductImages />
               </div>
               <div className="col-md-5 p-3">
-                <h4>title</h4>
-                <p className="text-muted bodySmal my-1">Category</p>
-                <p className="text-muted bodySmal my-1">Brand</p>
-                <p className="bodySmal mb-2 mt-1">shortDes</p>
-                <span>
-                  <strike class="text-secondary">$price</strike> $discountPrice
-                </span>
+                <h4>{Details[0]["title"]}</h4>
+                <p className="text-muted bodySmal my-1">
+                  Category: {Details[0]["category"]["categoryName"]}
+                </p>
+                <p className="text-muted bodySmal my-1">
+                  Brand: {Details[0]["brand"]["brandName"]}
+                </p>
+                <p className="bodySmal mb-2 mt-1">{Details[0]["shortDes"]}</p>
+                {Details[0]?.discount ? (
+                  <span>
+                    <strike className="text-secondary">
+                      {Details[0]?.price}
+                    </strike>
+                    ${Details[0]?.discountPrice}
+                  </span>
+                ) : (
+                  <span>${Details[0]?.price}</span>
+                )}
+
                 <div className="row">
                   <div className="col-4 p-2">
                     <label className="bodySmal">Size</label>
                     <select className="form-control my-2 form-select">
                       <option value="">Size</option>
+                      {Details[0]["details"]["size"]
+                        .split(",")
+                        .map((size, index) => (
+                          <option key={index}>{size}</option>
+                        ))}
                     </select>
                   </div>
                   <div className="col-4  p-2">
                     <label className="bodySmal">Color</label>
                     <select className="form-control my-2 form-select">
                       <option value="">Color</option>
+                      {Details[0]["details"]["color"]
+                        .split(",")
+                        .map((color, index) => (
+                          <option key={index}>{color}</option>
+                        ))}
                     </select>
                   </div>
                   <div className="col-4  p-2">
                     <label className="bodySmal">Quantity</label>
                     <div className="input-group my-2">
-                      <button className="btn btn-outline-secondary">-</button>
+                      <button onClick={decrementQunatity} className="btn btn-outline-secondary">-</button>
                       <input
+                      value={quantity}
                         type="text"
                         className="form-control bg-light text-center"
                         readOnly
                       />
-                      <button className="btn btn-outline-secondary">+</button>
+                      <button onClick={incrementQunatity} className="btn btn-outline-secondary">+</button>
                     </div>
                   </div>
                   <div className="col-4  p-2">
@@ -101,7 +134,9 @@ const Details = () => {
                   role="tabpanel"
                   aria-labelledby="Speci-tab"
                   tabIndex="0"
-                ></div>
+                >{
+                    parse(Details[0]['details']["des"])
+                }</div>
                 <div
                   className="tab-pane fade"
                   id="Review-tab-pane"
